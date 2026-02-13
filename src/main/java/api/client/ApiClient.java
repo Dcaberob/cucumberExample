24038464534fa7792764f91e7392b9d60151fb50
Dcaberob <cabernaldo010788@gmail.com>
2026-02-13 15:40:47 -0400

    start the project and add some files

---
+++ src/main/java/api/client/ApiClient.java
@@ -0,0 +1,11 @@
+package api.client;
+
+import org.openqa.selenium.devtools.v123.network.model.Request;
+
+import java.io.IOException;
+import java.net.http.HttpClient;
+import java.net.http.HttpResponse;
+
+public abstract class ApiClient {
+
+}
---
+++ src/main/java/api/client/RequestApi.java
@@ -0,0 +1,12 @@
+package api.client;
+
+
+import jakarta.ws.rs.core.MultivaluedHashMap;
+import jakarta.ws.rs.core.MultivaluedMap;
+
+import java.util.HashMap;
+import java.util.Map;
+
+public class RequestApi {
+
+}
