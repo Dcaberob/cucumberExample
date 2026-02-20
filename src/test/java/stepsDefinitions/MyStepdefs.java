3ef5bcc28cae05dfb8a1950b008d6284ed4d6d31
Dcaberob <cabernaldo010788@gmail.com>
2026-02-20 16:47:06 -0400

    add a new scenarios

--- src/test/java/stepsDefinitions/MyStepdefs.java
+++ src/test/java/stepsDefinitions/MyStepdefs.java
@@ -6,7 +6,9 @@ import api.client.RequestApi;
 import api.client.ResponseApi;
 import io.cucumber.java.en.Then;
 import io.cucumber.java.en.When;
+import jakarta.validation.constraints.AssertTrue;
 import org.glassfish.jersey.client.ClientRequest;
+import org.testng.Assert;
 
 import java.io.IOException;
 import java.net.http.HttpResponse;
@@ -26,19 +28,25 @@ public class MyStepdefs {
     @When("^I (GET|POST|PUT|DELETE) request '(.*)'$")
     public void iGetRequest(String method, String service ) throws IOException, InterruptedException {
         requestApi.setUrl(HOST + service);
-
+        ApiClient clientRequest = FactoryRequest.make(method);
+        System.out.println(requestApi.getUrl());
+        response = clientRequest.send(requestApi);
+    }
+    @When("^I (GET|POST|PUT|DELETE) request '(.*)' and send data$")
+    public void iGetRequestWithEmail(String method, String service,
+                                     Map<String, String> data ) throws IOException, InterruptedException {
+        requestApi.setUrl(HOST + service);
+        requestApi.setParams(data);
         ApiClient clientRequest = FactoryRequest.make(method);
 
-
-        System.out.println(requestApi.getUrl());
-
+        System.out.println(requestApi.getParams());
         response = clientRequest.send(requestApi);
-
-
     }
 
     @Then("The status code is {int}")
     public void theStatusCodeIs(int statusCode) {
+        Assert.assertEquals(response.statusCode(),statusCode);
+        System.out.println(response.body());
         System.out.println(response.statusCode());
     }
 }
--- src/test/resources/features/api/getRequest.feature
+++ src/test/resources/features/api/getRequest.feature
@@ -1,5 +1,14 @@
 Feature: Get Request
 
   Scenario: Get all products
-    When I GET request 'productList'
+    When I GET request 'productsList'
+    Then The status code is 200
+
+  Scenario: Get all Brands list
+    When I GET request 'brandsList'
+    Then The status code is 200
+
+  Scenario: Get User send to email
+    When I GET request 'getUserDetailByEmail' and send data
+    |email    |   cabernaldo010788@gmail.com|
     Then The status code is 200
